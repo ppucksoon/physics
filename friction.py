64d69b6811939611d6ec9e59408bdb9692136233
heeyeon cha <ckgmldus2@naver.com>
2023-01-02 14:51:27 +0900

    third commit

--- friction.py
+++ friction.py
@@ -1,3 +1,4 @@
+from functools import cache
 import pygame
 import math
 import time
@@ -8,72 +9,58 @@ pygame.display.set_caption("마찰력 가상 실험")
 WHITE = (255, 255, 255)
 GRAY = (200, 200, 200)
 BLACK = (0, 0 ,0)
-FPS = 60
+FPS = 1000
 size = (800, 800)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 done = False
 
-box_length = (50, 30)
-box = pygame.image.load('./img/box.png')
-box = pygame.transform.scale(box, box_length)
-
+@cache
 def font(size):
     return pygame.font.Font('./font/NotoSansKR-Medium.otf', size)
 
-def blitRotate(image, pos, originPos, angle): 
-    w, h       = image.get_size()
-    box        = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]] 
-    box_rotate = [p.rotate(angle) for p in box] 
-    min_box    = (min(box_rotate, key=lambda p: p[0])[0], min(box_rotate, key=lambda p: p[1])[1]) 
-    max_box    = (max(box_rotate, key=lambda p: p[0])[0], max(box_rotate, key=lambda p: p[1])[1]) 
- 
-    pivot        = pygame.math.Vector2(originPos[0], -originPos[1]) 
-    pivot_rotate = pivot.rotate(angle) 
-    pivot_move   = pivot_rotate - pivot 
- 
-    origin = ((pos[0] - originPos[0] + min_box[0] - pivot_move[0]), (pos[1] - originPos[1] - max_box[1] + pivot_move[1])) 
- 
-    rotated_image = pygame.transform.rotate(image, angle) 
-    return (rotated_image, origin)
-
-def getRotate(cri_pos, pos):
-    deltaX = pos[0] - cri_pos[0]
-    deltaY = pos[1] - cri_pos[1]
-    value = math.degrees(math.atan2(deltaX, deltaY)) -90
-    if value < 0:
-        value += 360
-    return value
-
-board_length = 0.564
-tri_degree = 20
-tri_theta = math.radians(tri_degree)
-tri_pos = [(100, 700), (700, 700), [0, 0]]
+board = 0.564       # 빗면의 길이
+tri_degree = 20     # 빗면의 각도(˚)
+tri_theta = math.radians(tri_degree)    # 빗면의 각도(rad)
+tri_pos = [[100, 750], [700, 750], [0, 0]]
 tri_pos[2] = [tri_pos[1][0], tri_pos[1][1] - math.tan(tri_theta)*(tri_pos[1][0]-tri_pos[0][0])]
-rate = math.sqrt(math.pow(tri_pos[0][0]-tri_pos[1][0], 2) + math.pow(tri_pos[1][1]-tri_pos[2][1], 2))/board_length
-# rate = 1
-mass = 0.25
-gravity = 9.8
-mu_s = 0.3639702342
-mu_k = 0.1632653061
-r = getRotate(tri_pos[0], tri_pos[2])
-diagonal_length = math.sqrt(math.pow(box_length[0], 2) + math.pow(box_length[1], 2))
-diagonal_theta = math.atan(box_length[1] / box_length[0])
-box_rb_pos = [tri_pos[2][0], tri_pos[2][1]]
-stop = False
-acceleration = gravity*(math.sin(tri_theta) - mu_s*math.cos(tri_theta))
-if acceleration <= 0:
-    acceleration = 0
-velocity = acceleration
+mass = 0.25     # 물체의 질량
+gravity = 9.8   # 중력 가속도
+mu_s = math.tan(math.radians(15))   # 정시 마찰계수
+mu_k = 0.4/(0.25*9.8)               # 운동 마찰계수
+df = 1 / FPS    # 프레임당 차지하는 시간(s)
 
 start_t = time.time()
-flag = True
+stop_t = time.time()
+
+def reset():
+    global acceleration, stop, no_move, velocity, object_delta_pos, \
+        object_pos, tri_degree, tri_theta, tri_pos, gravity, mu_s, rate
+    stop = False
+    no_move = False
+    tri_theta = math.radians(tri_degree)
+    acceleration = gravity*(math.sin(tri_theta) - mu_s*math.cos(tri_theta))
+    if acceleration <= 0:
+        acceleration = 0
+        stop = True
+        no_move = True
+    velocity = acceleration*df
+    object_delta_pos = 0
+    object_pos = [0, 0]
+    tri_pos[2] = [tri_pos[1][0], tri_pos[1][1] - math.tan(tri_theta)*(tri_pos[1][0]-tri_pos[0][0])]
+    rate = math.sqrt(math.pow(tri_pos[0][0]-tri_pos[1][0], 2) + math.pow(tri_pos[1][1]-tri_pos[2][1], 2))/board
+    # 실제 길이와 픽셀 수 사이의 비율
+
+reset()
 
 while not done:
     clock.tick(FPS)
     screen.fill(BLACK)
 
-    delta_time = time.time() - start_t
+    try:
+        df = 1 / clock.get_fps()
+    except:
+        df = 1 / FPS
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -83,54 +70,38 @@ while not done:
                 tri_degree += 10
             if event.key == pygame.K_w:
                 tri_degree += 1
-            if event.key == pygame.K_e:
-                tri_degree += 0.1
-            if event.key == pygame.K_r:
-                tri_degree += 0.01
             if event.key == pygame.K_a:
                 tri_degree -= 10
             if event.key == pygame.K_s:
                 tri_degree -= 1
-            if event.key == pygame.K_d:
-                tri_degree -= 0.1
-            if event.key == pygame.K_f:
-                tri_degree -= 0.01
             if tri_degree <= 0:
                 tri_degree = 0
-            tri_theta = math.radians(tri_degree)
-            tri_pos[2] = [tri_pos[1][0], tri_pos[1][1] - math.tan(tri_theta)*(tri_pos[1][0]-tri_pos[0][0])]
-            r = getRotate(tri_pos[0], tri_pos[2])
-            box_rb_pos = [tri_pos[2][0], tri_pos[2][1]]
-            delta_time = 2
-            stop = True
 
-    if delta_time >= 0.01 and not stop:
-        # start_t = time.time()
-        if acceleration > 0:
-            acceleration = gravity*(math.sin(tri_theta) - mu_k*math.cos(tri_theta))
-        velocity += acceleration*0.01
-        box_rb_pos[0] -= velocity*math.cos(tri_theta)
-        box_rb_pos[1] += velocity*math.sin(tri_theta)
-        flag = True
+            reset()
+            start_t = time.time()
     
-    if box_rb_pos[0] - box_length[0]*math.cos(tri_theta) <= tri_pos[0][0]:
+    if not stop:
+        acceleration = gravity*(math.sin(tri_theta) - mu_k*math.cos(tri_theta))
+        velocity += acceleration*df
+        object_delta_pos += velocity*df
+        object_pos[0] += velocity*df*math.cos(tri_theta)*rate
+        object_pos[1] += velocity*df*math.sin(tri_theta)*rate
+
+    if object_delta_pos >= board and not stop:
         stop = True
-        if flag:
-            print(delta_time)
-            flag = False
-
-    if stop and delta_time >= 1.5:
+        stop_t = time.time()
+        print(f'소요 시간 : {time.time() - start_t} 초')
+    
+    if no_move:
+        pass
+    elif stop and time.time() - stop_t >= 1:
+        object_delta_pos = 0
+        object_pos = [0, 0]
+        start_t = time.time()
         stop = False
-        box_rb_pos = [tri_pos[2][0], tri_pos[2][1]]
-        acceleration = gravity*(math.sin(tri_theta) - mu_s*math.cos(tri_theta))
-        if acceleration <= 0:
-            acceleration = 0
-        velocity = acceleration
+        reset()
 
-    box_pos = [(-diagonal_length/2*math.cos(tri_theta - diagonal_theta) + box_rb_pos[0]), \
-        (diagonal_length/2*math.sin(tri_theta - diagonal_theta) + box_rb_pos[1])]
-    rotate_box = blitRotate(box, box_pos, (box_length[0]//2, box_length[1]//2), r)
-    screen.blit(rotate_box[0], rotate_box[1])
+    pygame.draw.circle(screen, WHITE, (tri_pos[2][0] - object_pos[0], tri_pos[2][1] + object_pos[1]-15), 15)
     pygame.draw.polygon(screen, GRAY, tri_pos)
 
     tri_degree_txt = font(30).render(f"tri_degree : {tri_degree:.2f}", True, WHITE)
